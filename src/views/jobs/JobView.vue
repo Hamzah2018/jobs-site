f7705da04cd5a1b18bb3be265aea33977e1d86b1
Hamzah Al-Hagi <ha.773774142@gmail.com>
2023-09-15 17:57:46 +0300

    Redesing job filter

--- src/views/jobs/JobView.vue
+++ src/views/jobs/JobView.vue
@@ -1,18 +1,13 @@
 
 <template>
-  <section class="banner"
+  <section class="banner mb-md-6 mb-ms-6"
    :style="{ backgroundImage: 'url(' + image + ')' }"
    >
-    <div class="container-lg">
+    <div class="container-lg ">
         <div class="row pt-5 pb-5">
-            <div class="col-12">
-                <div class="p-lg-5 p-md-5 text-lg-start text-md-start text-sm-center text-center">
-                    <h1 class="fs-1 fw-bold text-light mt-5">Hr-jobs</h1>
-                    <p class="text-white-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dolorum itaque natus numquam</p>
-                </div>
-            </div>
+         
             <!-- Search -->
-            <div class="card py-2 shadow m-auto position-relative search" style="width: 75%;">
+            <section class="card py-2 shadow m-auto position-relative search " >
                 <div class="row">
                     <div class="col-lg-1 col-md-1 col-sm-1 col-2">
                         <div class="bg-20 bg-warning  d-flex logo border  shadow-sm align-items-center justify-content-center">
@@ -25,83 +20,70 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </section>
+            <section class="col-12 card py-3 shadow m-auto my-4 position-relative search" styel="background-color:#FFC107">
+                <div class="row">
+                    <div class="col-md-4">
+                      <select class="form-control">
+                        <option  v-for="j of resultQuery" :key="j.id" :value="j.country" selected > {{ j.country }} </option>
+                        <option value="" selected>City</option>
+                      </select>
+                    </div>
+                    <div class="col-md-4">
+                      <select class="form-control" aria-label="Default select example">
+                        <option  v-for="j of resultQuery" :key="j.id" :value="j.company" selected > {{ j.company }} </option>
+                        <option value="" selected  >Company</option>
+                      </select>
+                    </div>
+                    <div class="col-md-4">
+                      <select class="form-control">
+                        <option  v-for="j of resultQuery" :key="j.id" :value="j.type" selected > {{ j.type }} </option>
+                        <option value="" selected>Job Type</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="row mt-2">
+                    <div class="col-md-4">
+                      <select class="form-control">
+                        <option  v-for="j of resultQuery" :key="j.id" :value="j.type" selected > {{ j.type }} </option>
+                        <option value="" selected>Work Type</option>
+                      </select>
+                    </div>
+                    <div class="col-md-4">
+                      <select class="form-control">
+                        <option value="">Time</option>
+                        <option>Last time</option>
+                        <option>Today</option>
+                        <option>This week</option>
+                        <option>This month</option>
+                      </select>
+                    </div>
+                    <div class="col-md-4">
+                      <select class="form-control">
+                        <option  v-for="j of resultQuery" :key="j.id" :value="j.price" selected > {{ j.price }} </option>
+                        <option value="" selected>Price</option>
+                      </select>
+                    </div>
+                  </div>
+
+            </section>
         </div>
+      
     </div>
 </section>
 <!-- end Search  -->
 
-<main>
-  <div class="container-lg">
-      <div class="row pt-4 mt-lg-5 mt-md-4 mt-0">
+<main class="my-md-5 my-ms-5">
+  <div class="container-lg ">
+      <div class="row pt-4 mt-lg-3 mt-md-4 mt-5">
           <!-- header of page -->
           
-          <!-- Filter Section -->
-          <section class="Filter col-lg-4 col-md-4 col-sm-12 mb-3">
-              <div class="card shadow p-3">
-                  <div class="card-header bg-transparent mb-4">
-                      <h1 class="fs-4 fw-bold text-primary">
-                          <i class="bi bi-funnel fs-5 "></i>
-                          <span>Filter</span>
-                      </h1>
-                  </div>
-                  <form class="d-flex flex-column gap-3">
-                      <div class="col-12 input-group flex-nowrap position-relative">
-                          <div class="position-absolute top-50 end-0 translate-middle-y" style="z-index: 999; margin-right: 14px;">
-                              <i class="bi bi-search text-black-50"></i>
-                          </div>
-                          <input type="search" id="" class="form-control rounded-pill" placeholder="search" aria-label="email" aria-describedby="addon-wrapping">
-                      </div>
-                      <div>
-                          <h6 class="text-primary">الشركة</h6>
-                          <select class="form-select" aria-label="Default select example">
-                              <option selected>كارت بلس</option>
-                              <option value="1">One</option>
-                              <option value="2">Two</option>
-                              <option value="3">Three</option>
-                          </select>
-                      </div>
-                      <div>
-                          <h6 class="text-primary">المدينة</h6>
-                          <select class="form-select" aria-label="Default select example">
-                              <option selected>صنعاء</option>
-                              <option value="1">One</option>
-                              <option value="2">Two</option>
-                              <option value="3">Three</option>
-                          </select>
-                      </div>
-                      <div>
-                          <h6 class="text-primary">التصنيفات</h6>
-                          <div class="form-check mb-2">
-                              <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                              <label class="form-check-label" for="flexCheckDefault">
-                                  Default checkbox
-                              </label>
-                          </div>
-                          <div class="form-check mb-2">
-                              <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                              <label class="form-check-label" for="flexCheckDefault">
-                                  Default checkbox
-                              </label>
-                          </div>
-                          <div class="form-check mb-2">
-                              <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                              <label class="form-check-label" for="flexCheckDefault">
-                                  Default checkbox
-                              </label>
-                          </div>
-                      </div>
-                      <div>
-                          <h6 class="text-primary">المهارات</h6>
-                      </div>
-                  </form>
-              </div>
-          </section>
+      
           <!-- All Job Section -->
-          <section class="col-lg-8 col-md-8 col-sm-12">
+          <section class="col-lg-12 col-md-8 col-sm-12">
               <!-- Single Job -->
 
-              <article v-for="r of resultQuery" :key="r.id"  class="card p-lg-3 p-md-2 p-3 mb-3 shadow-sm search-job" role="button">
+              <article v-for="r of resultQuery" :key="r.id"  class="card p-lg-3 p-md-2 p-3 mb-2 shadow-sm search-job border-4 border-success-subtle" role="button">
                   <div class="card-title d-flex justify-content-between align-items-center m-0">
                       <div class="px-2 s">
                           <h3 class="mb-0"><a class="text-primary fs-5 fw-bold" href="../templates/detailsJob.html">{{r.title}}</a></h3>
@@ -164,6 +146,8 @@ export default {
   data() {
     return {
       searchQuery: null,
+      filterQuery: null,
+
       jobs:[
                 {id: 1, title: 'Systems Engineer, Microsoft Services' , company:'Microsoft' , country:'Yemen' , body:'Our Culture: At STS we are proud of our culture and how it drives everything.' , type:'Full-time',price:"1500$ - 2500$",data:'March 03' ,isOffer: true}, 
                 {id: 2, title: 'javascript for dummies, Microsoft Services', company:'Google' , country:'Eypt' ,body:'Our Culture: At STS we are proud of our culture and how it drives everything.',type:'Full-time',price:"3500$ - 2500$" ,data:'Feb 01'  ,isOffer: true}, 
@@ -185,7 +169,20 @@ export default {
       } else {
         return this.jobs;
       }
-    }
+    },
+    resultCopany() {
+      if (this.filterQuery) {
+        return this.jobs.filter(item => {
+          return this.filterQuery
+            .toLowerCase()
+            .split(" ")
+            .every(v => item.company.toLowerCase().includes(v));
+        });
+      } else {
+        return this.jobs;
+      }
+    },
+
   }
 };
 
@@ -193,9 +190,8 @@ export default {
 
 <style>
 .banner{
-   
-   height: 50vh;
-   background-color: rgb(211, 235, 211);
+  height: 50vh;
+  background-color: rgb(211, 235, 211);
   /* background-image: '../../assets/imges/loginback.png';*/
 }
 </style>
